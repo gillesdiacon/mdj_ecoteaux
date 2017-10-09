a5e7e3dd0111e80d48b3e98cbb684d2daeade0e6
gillesdiacon <gillesdiacon@users.noreply.github.com>
2017-10-09 19:38:32 +0200

    jquery prototype

--- js/agenda.js
+++ js/agenda.js
@@ -1,95 +1,107 @@
-$(function() {
-    var agendaElem = $('#calendar');
-    agendaElem.empty();
-    
-    var occupiedDays = [];
-    var dayNames = ["lun", "mar", "mer", "jeu", "ven", "sam", "dim"];
-    var monthNames = ["Janvier", "FÃ©vrier", "Mars", "Avril", "Mai", "Juin", "Juillet", "AoÃ»t", "Septembre", "Octobre", "Novembre", "DÃ©cembre"];
-    
-    var newDate = new Date();
-    var day = newDate.getDate();
-    var month = newDate.getMonth();
-    var year = newDate.getFullYear();
-    var today = new Date(newDate.getTime());
-    today.setHours(0,0,0,0);
-    
-    var firstDate = new Date(year, month, 1);
-    
-    var currentDate = new Date(firstDate.getTime());
-    var lastDate = new Date(year, month + 1, 0);
-    
-    // display agenda header
-    var headerRow = $(document.createElement('div'));
-    headerRow.addClass('row');
-    for(var d = 0; d < 7; d++){
-        var headerCell = $(document.createElement('div'));
-        headerCell.addClass('col border border-left-0 border-right-0 border-top-0 border-dark');
-        headerCell.text(dayNames[d]);
-        headerRow.append(headerCell);
-    }
-    agendaElem.append(headerRow);
-
-    while (currentDate.getDay() != 1) {
-        currentDate.setDate(currentDate.getDate() - 1);
-    }
-
-    while (currentDate <= lastDate) {
-        var row = $(document.createElement('div'));
-        row.addClass('row');
-
-        do {
-            var cell = $(document.createElement('div'));
-            cell.addClass('col border border-top-0 border-dark');
-            if (currentDate.getDay() != 1) {cell.addClass('border-left-0');}
-            cell.addClass('day');
-            
-            if (currentDate < firstDate) {
-                cell.addClass('previous');
-            } else if(currentDate > lastDate) {
-                cell.addClass('futur');
-            } else {
-                if(occupiedDays.length > 0) {
-                    for(var o in occupiedDays){
-                        if(currentDate.getTime() == occupiedDays[o].getTime()) {
-                            cell.addClass('occupied');
-                            break;
-                        }
-                    }
+ (function($) {
+	var Calendar = function(element, options) {
+		this.agendaElem = element;
+        
+        var newDate = new Date();
+        var day = newDate.getDate();
+        var month = newDate.getMonth();
+        var year = newDate.getFullYear();
+        
+        this.today = new Date(newDate.getTime());
+        this.today.setHours(0,0,0,0);
+        
+        this.firstDate = new Date(year, month, 1);
+        this.lastDate = new Date(year, month + 1, 0);
+        
+        this.dayNames = ["lun", "mar", "mer", "jeu", "ven", "sam", "dim"];
+        this.monthNames = ["Janvier", "FÃ©vrier", "Mars", "Avril", "Mai", "Juin", "Juillet", "AoÃ»t", "Septembre", "Octobre", "Novembre", "DÃ©cembre"];
+		
+		this.render();
+	};
+ 
+	Calendar.prototype = {
+		constructor: Calendar,
+		getGoogleEvents: function() {
+			var events = [];
+			
+            $.ajax({
+                url: "https://content.googleapis.com/calendar/v3/calendars/m6t1df6s0fbnuhulearfu44cmo%40group.calendar.google.com/events?key=AIzaSyAuYs5D3rOhEiOI8LWPJpFBoCJNJbvWJpY",
+                data: {
+                    "timeMin": this.firstDate.toISOString(),
+                    "timeMax": this.lastDate.toISOString()
                 }
-                
-                // today
-                if(currentDate.getTime() == today.getTime()){
-                    cell.addClass('bg-warning');
-                }
-                
-                cell.text(currentDate.getDate());
+            }).then(function(data) {
+               console.log(data);
+            });
+			
+			return events;
+		},
+        render: function() {
+            this.agendaElem.empty();
+    
+            var occupiedDays = this.getGoogleEvents();
+
+            // display agenda header
+            var headerRow = $(document.createElement('div'));
+            headerRow.addClass('row');
+            for(var d = 0; d < 7; d++){
+                var headerCell = $(document.createElement('div'));
+                headerCell.addClass('col border border-left-0 border-right-0 border-top-0 border-dark');
+                headerCell.text(this.dayNames[d]);
+                headerRow.append(headerCell);
             }
-            
-            row.append(cell);
-            
-            currentDate.setDate(currentDate.getDate() + 1);
-        } while(currentDate.getDay() != 1)
-    
-        agendaElem.append(row);
-    }
-});
+            this.agendaElem.append(headerRow);
 
-$(document).ready(function() {
-    var newDate = new Date();
-    var day = newDate.getDate();
-    var month = newDate.getMonth();
-    var year = newDate.getFullYear();
-    
-    var firstDate = new Date(year, month, 1);
-    var lastDate = new Date(year, month + 1, 0);
-    
-    $.ajax({
-        url: "https://content.googleapis.com/calendar/v3/calendars/m6t1df6s0fbnuhulearfu44cmo%40group.calendar.google.com/events?key=AIzaSyAuYs5D3rOhEiOI8LWPJpFBoCJNJbvWJpY",
-        data: {
-            "timeMin": firstDate.toISOString(),
-            "timeMax": lastDate.toISOString()
+            var currentDate = new Date(this.firstDate.getTime());
+            while (currentDate.getDay() != 1) {
+                currentDate.setDate(currentDate.getDate() - 1);
+            }
+
+            while (currentDate <= this.lastDate) {
+                var row = $(document.createElement('div'));
+                row.addClass('row');
+
+                do {
+                    var cell = $(document.createElement('div'));
+                    cell.addClass('col border border-top-0 border-dark');
+                    if (currentDate.getDay() != 1) {cell.addClass('border-left-0');}
+                    cell.addClass('day');
+                    
+                    if (currentDate < this.firstDate) {
+                        cell.addClass('previous');
+                    } else if(currentDate > this.lastDate) {
+                        cell.addClass('futur');
+                    } else {
+                        if(occupiedDays.length > 0) {
+                            for(var o in occupiedDays){
+                                if(currentDate.getTime() == occupiedDays[o].getTime()) {
+                                    cell.addClass('occupied');
+                                    break;
+                                }
+                            }
+                        }
+                        
+                        // today
+                        if(currentDate.getTime() == this.today.getTime()){
+                            cell.addClass('bg-warning');
+                        }
+                        
+                        cell.text(currentDate.getDate());
+                    }
+                    
+                    row.append(cell);
+                    
+                    currentDate.setDate(currentDate.getDate() + 1);
+                } while(currentDate.getDay() != 1)
+            
+                this.agendaElem.append(row);
+            }
         }
-    }).then(function(data) {
-       console.log(data);
-    });
-}); 
+    }
+    
+    $.fn.calendar = function (options) {
+		var calendar = new Calendar($(this) ,options);
+		$(this).data('calendar', calendar);
+		return calendar;
+	}
+ }(window.jQuery));
